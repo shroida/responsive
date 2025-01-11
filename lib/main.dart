a4fba5417637f864446286de182ef9c12f7ef9a3
shroida <shroida012@gmail.com>
2025-01-11 03:15:36 +0200

    add tablig layout UI

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
           if (constrains.maxWidth < 600) {
             return const MobileLayout();
           } else if (constrains.maxWidth > 600 && constrains.maxWidth < 900) {
-            return const Text('tablit');
+            return const TabLayout();
           } else {
             return const Text('desktop');
           }
@@ -86,6 +86,47 @@ class ShowDetails extends StatelessWidget {
         width: 700,
         child: Text(numberDetails.toString()),
       ),
+    );
+  }
+}
+
+class TabLayout extends StatefulWidget {
+  const TabLayout({super.key});
+
+  @override
+  TabLayoutState createState() => TabLayoutState();
+}
+
+class TabLayoutState extends State<TabLayout> {
+  int number = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, item) {
+                return GestureDetector(
+                  onTap: () {
+                    number = item + 1;
+                    setState(() {});
+                  },
+                  child: Container(
+                    margin:
+                        const EdgeInsets.only(bottom: 15, right: 20, left: 20),
+                    color: const Color.fromARGB(255, 64, 106, 254),
+                    height: 50,
+                    width: 200,
+                    child: Text('${item + 1}'),
+                  ),
+                );
+              }),
+        ),
+        Expanded(child: Text(number.toString()))
+      ],
     );
   }
 }
