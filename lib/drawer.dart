378d79ad5d6eae3e92987352f12fb2ed5f5bb1df
shroida <shroida012@gmail.com>
2025-01-14 17:40:20 +0200

    add fontawesomeflutter to pubspec

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomDrawer extends StatelessWidget {
 const CustomDrawer({ super.key });
 
   @override
   Widget build(BuildContext context){
-    return const Placeholder();
+    return const Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(child: Icon(FontAwesomeIcons.solidHeart))
+        ],
+      ),
+    );
   }
 }
